2a31bb1a87d0147081e85ca235022624cda0d6f0
dmentizis <dmentizis@gmail.com>
2025-11-29 00:17:19 +0200

    Implemented Domain Layer

---
+++ Domain/CityWeather.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class CityWeather
+    {
+        public string? CityName { get; set; }
+        public string? WeatherDescription { get; set; }
+        public double? Temperature { get; set; }
+        public double? TemperatureFeels { get; set; }
+        public int? Humidity { get; set; }
+        public double? WindSpeed { get; set; }
+    }
+}
---
+++ Domain/NewsArticle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class NewsArticle
+    {
+        public string? SourceName { get; set; }
+        public string? Author { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+    }
+}
